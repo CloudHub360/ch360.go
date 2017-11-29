a0acd8befe9e03059e5a167954b2f38af7ef15a2
Alastair Smith <github@alastairsmith.me.uk>
2017-11-29 15:04:01 +0000

    Test the version info output format is correct

--- test/ch360-version.Tests.ps1
+++ test/ch360-version.Tests.ps1
@@ -10,4 +10,8 @@ Describe "ch360 --version" {
   It "should output build number" {
     ch360 --version | Should -Match ".*:(\d+)"
   }
+
+  It "should output the supported format" {
+    ch360 --version | Should -Match "\d{4}\.\d{2}\.\d{2}-([a-f0-9]{7,}):(\d+)"
+  }
 }
