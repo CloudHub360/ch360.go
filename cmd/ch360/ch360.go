48da6411402217830d624f0223141967d4582820
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-01 14:48:11 +0000

    Get token using credentials and print to console

--- cmd/ch360/ch360.go
+++ cmd/ch360/ch360.go
@@ -5,6 +5,11 @@ import (
 
 	"github.com/CloudHub360/ch360.go"
 	"github.com/docopt/docopt-go"
+	"fmt"
+	"github.com/CloudHub360/ch360.go/authtoken"
+	"net/http"
+	"time"
+	"os"
 )
 
 func main() {
@@ -13,11 +18,35 @@ func main() {
 Usage:
   ch360 -h | --help
   ch360 --version
+  ch360 --id=<id> --secret=<secret>
 
 Options:
-  -h --help     Show this help message.
-  --version     Show version.`
+  -h --help          Show this help message.
+  --version          Show version.
+  --id=<id>          API Client ID
+  --secret=<secret>  API Client secret`
 
-	docopt.Parse(usage, nil, true, ch360.Version, false)
+	args, err := docopt.Parse(usage, nil, true, ch360.Version, false)
 
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	id := args["--id"].(string)
+	secret := args["--secret"].(string)
+
+	var httpClient = &http.Client{
+		Timeout: time.Minute * 5,
+	}
+
+	tokenGetter := authtoken.NewHttpGetter(id, secret, httpClient, authtoken.ApiAddress)
+
+	token, err := tokenGetter.Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(token)
 }
