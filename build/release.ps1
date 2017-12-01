af658b43363e81e91269844a37df611272ee8b57
Alastair Smith <github@alastairsmith.me.uk>
2017-12-01 10:46:32 +0000

    Create GitHub release
    Invoke the create release API method.

--- build/release.ps1
+++ build/release.ps1
@@ -5,8 +5,22 @@ param(
     [string]$ReleaseNotes
 )
 
-Task Release {
+$GITHUB_RELEASES_URI = "https://api.github.com/repos/CloudHub360/ch360.go/releases"
 
+Task Release {
+    $releaseMetadata = [PSCustomObject] @{
+        "tag_name" = "v$VersionString"
+        "target_commitish" = $Commit
+        "name" = $VersionString
+        "body" = $ReleaseNotes
+    }
+
+    Invoke-RestMethod `
+        -Method POST `
+        -Headers @{ "Authorization"="token $GitHubToken" } `
+        -Uri $GITHUB_RELEASES_URI `
+        -ContentType "application/json" `
+        -Body (ConvertTo-Json $releaseMetadata)
 }
 
 Task . Release
