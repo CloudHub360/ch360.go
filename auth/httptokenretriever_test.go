56675bc70459296c840f230b9d78cf80976a8801
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-05 13:47:14 +0000

    Remove more duplication in tests

--- auth/httptokenretriever_test.go
+++ auth/httptokenretriever_test.go
@@ -26,15 +26,21 @@ func AnHttpResponse(body []byte, status int) *http.Response {
 
 type HttpTokenRetrieverSuite struct {
 	suite.Suite
-	sut 				*HttpTokenRetriever
+	sut                 *HttpTokenRetriever
 	mockHttpClient      *mocks.FormPoster
 	mockResponseChecker *mocks.Checker
+	validTokenValue		string
+	validTokenBody      string
+	validTokenResponse	*http.Response
 }
 
 func (suite *HttpTokenRetrieverSuite) SetupTest() {
 	suite.mockHttpClient = new(mocks.FormPoster)
 	suite.mockResponseChecker = new(mocks.Checker)
 	suite.sut = NewHttpTokenRetriever(fakeClientId, fakeClientSecret, suite.mockHttpClient, "notused", suite.mockResponseChecker)
+	suite.validTokenValue = `tokenvalue`
+	suite.validTokenBody = `{"access_token": "` + suite.validTokenValue + `"}`
+	suite.validTokenResponse = AnHttpResponse([]byte(suite.validTokenBody), 200)
 }
 
 func TestExampleTestSuite(t *testing.T) {
@@ -43,11 +49,8 @@ func TestExampleTestSuite(t *testing.T) {
 
 func (suite *HttpTokenRetrieverSuite) Test_HttpTokenRetriever_Sends_Client_Id_And_Secret() {
 	// Arrange
-	expectedResponseBody := `{"access_token": "tokenvalue"}`
-	response := AnHttpResponse([]byte(expectedResponseBody), 200)
-
-	suite.mockHttpClient.On("PostForm", mock.Anything, mock.Anything).Return(response, nil)
-	suite.mockResponseChecker.On("Check", mock.Anything, mock.Anything).Return([]byte(expectedResponseBody), nil)
+	suite.mockHttpClient.On("PostForm", mock.Anything, mock.Anything).Return(suite.validTokenResponse, nil)
+	suite.mockResponseChecker.On("Check", mock.Anything, mock.Anything).Return([]byte(suite.validTokenBody), nil)
 
 	// Act
 	suite.sut.RetrieveToken()
@@ -72,17 +75,14 @@ func Test_HttpTokenRetriever_Returns_Error_On_HttpClient_Error(t *testing.T) {
 
 func (suite *HttpTokenRetrieverSuite) Test_HttpTokenRetriever_Passes_Response_To_Checker() {
 	// Arrange
-	expectedResponseBody := `{"access_token": "tokenvalue"}`
-	response := AnHttpResponse([]byte(expectedResponseBody), 200)
-
-	suite.mockHttpClient.On("PostForm", mock.Anything, mock.Anything).Return(response, nil)
-	suite.mockResponseChecker.On("Check", mock.Anything, mock.Anything).Return([]byte(expectedResponseBody), nil)
+	suite.mockHttpClient.On("PostForm", mock.Anything, mock.Anything).Return(suite.validTokenResponse, nil)
+	suite.mockResponseChecker.On("Check", mock.Anything, mock.Anything).Return([]byte(suite.validTokenBody), nil)
 
 	// Act
 	suite.sut.RetrieveToken()
 
 	// Assert
-	suite.mockResponseChecker.AssertCalled(suite.T(), "Check", response, 200)
+	suite.mockResponseChecker.AssertCalled(suite.T(), "Check", suite.validTokenResponse, 200)
 }
 
 func (suite *HttpTokenRetrieverSuite) Test_HttpTokenRetriever_Returns_Error_On_ResponseChecker_Error() {
@@ -102,18 +102,15 @@ func (suite *HttpTokenRetrieverSuite) Test_HttpTokenRetriever_Returns_Error_On_R
 
 func (suite *HttpTokenRetrieverSuite) Test_HttpTokenRetriever_Parses_Token_Response() {
 	// Arrange
-	expectedResponseBody := `{"access_token": "tokenvalue"}`
-	response := AnHttpResponse([]byte(expectedResponseBody), 200)
-
-	suite.mockHttpClient.On("PostForm", mock.Anything, mock.Anything).Return(response, nil)
-	suite.mockResponseChecker.On("Check", mock.Anything, mock.Anything).Return([]byte(expectedResponseBody), nil)
+	suite.mockHttpClient.On("PostForm", mock.Anything, mock.Anything).Return(suite.validTokenResponse, nil)
+	suite.mockResponseChecker.On("Check", mock.Anything, mock.Anything).Return([]byte(suite.validTokenBody), nil)
 
 	// Act
 	token, err := suite.sut.RetrieveToken()
 
 	// Assert
 	assert.Nil(suite.T(), err)
-	assert.Equal(suite.T(), "tokenvalue", token)
+	assert.Equal(suite.T(), suite.validTokenValue, token)
 }
 
 func (suite *HttpTokenRetrieverSuite) Test_HttpTokenRetriever_Returns_Err_On_Invalid_Json() {
