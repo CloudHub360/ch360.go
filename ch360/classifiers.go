ffaaf3a763103d508671ddd5cf04496ab684900c
Phil Richards <phil@plottwist.io>
2017-12-07 14:31:25 +0000

    Added `ClassifiersClient.DeleteClassifier`
    And associated test. ClassifiersClient tests tidied up too.

--- ch360/classifiers.go
+++ ch360/classifiers.go
@@ -7,19 +7,24 @@ type ClassifiersClient struct {
 	requestSender HttpDoer
 }
 
-func (client *ClassifiersClient) classifiersUrl() string {
-	return "/classifiers"
-}
-
-func (client *ClassifiersClient) CreateClassifier(name string) error {
-	request, err := http.NewRequest("POST",
-		client.baseUrl + client.classifiersUrl() + "/" + name,
-			nil)
+func (client *ClassifiersClient) issueRequest(method string, classifierName string) error {
+	request, err := http.NewRequest(method,
+		client.baseUrl + "/classifiers/" + classifierName,
+		nil)
 
 	if err != nil {
 		return err
 	}
 
 	_, err = client.requestSender.Do(request)
+
 	return err
+}
+
+func (client *ClassifiersClient) CreateClassifier(name string) error {
+	return client.issueRequest("POST", name)
+}
+
+func (client *ClassifiersClient) DeleteClassifier(name string) error {
+	return client.issueRequest("DELETE", name)
 }
