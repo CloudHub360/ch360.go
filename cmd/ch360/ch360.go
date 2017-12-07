1e7fe857157d7a4d89b3ee9e60a6143ddadd3149
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-07 22:37:45 +0000

    Use Delete Classifier command

--- cmd/ch360/ch360.go
+++ cmd/ch360/ch360.go
@@ -47,7 +47,8 @@ Options:
 		err = apiClient.Classifiers.CreateClassifier(classifierName)
 	} else {
 		fmt.Printf("Deleting classifier '%s'... ", classifierName)
-		err = apiClient.Classifiers.DeleteClassifier(classifierName)
+		cmd := commands.NewDeleteClassifier(apiClient.Classifiers)
+		err = cmd.Execute(classifierName)
 	}
 
 	if err != nil {
