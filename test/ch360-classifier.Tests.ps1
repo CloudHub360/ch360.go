70e0f16e184bab95ccb06595fd2f2d59cf01157b
Alastair Smith <github@alastairsmith.me.uk>
2017-12-08 11:56:26 +0000

    Ensure created classifiers are cleaned up
    When running the smoke tests, the create classifier tests leave a
    classifier called `test-classifier` hanging around, which then affects
    the running of the delete classifier tests: if the create classifier
    tests haven't run, the delete classifier test fails; if it has, then the
    test succeeds.
    
    Cleaning up the classifiers after the test requires the delete
    classifier test to create the classifier before attempting to delete it.

--- test/ch360-classifier.Tests.ps1
+++ test/ch360-classifier.Tests.ps1
@@ -3,15 +3,16 @@ param(
     [String]$ClientSecret
 )
 
+$classifierName = "test-classifier"
+
 Describe "ch360 delete classifier" {
     It "should output success" {
-        $classifierName = "test-classifier"
+        ch360 create classifier $classifierName --id="$ClientId" --secret="$ClientSecret" | Should -Be "Creating classifier '$classifierName'... [OK]"
         ch360 delete classifier $classifierName --id="$ClientId" --secret="$ClientSecret" | Should -Be "Deleting classifier '$classifierName'... [OK]"
         $LASTEXITCODE | Should -Be 0
     }
 
     It "should output failure when client id or secret are invalid" {
-        $classifierName = "test-classifier"
         ch360 delete classifier $classifierName --id="invalid-id" --secret="$ClientSecret" | Should -Be "Deleting classifier '$classifierName'... [FAILED]"
         $LASTEXITCODE | Should -Be 1
     }
@@ -19,18 +20,13 @@ Describe "ch360 delete classifier" {
 
 Describe "ch360 create classifier" {
     It "should output success when creating a new classifier" {
-        $classifierName = "test-classifier"
         # Run delete classifier first to ensure it's not already present
         ch360 delete classifier $classifierName --id="$ClientId" --secret="$ClientSecret"
         ch360 create classifier $classifierName --id="$ClientId" --secret="$ClientSecret" | Should -Be "Creating classifier '$classifierName'... [OK]"
         $LASTEXITCODE | Should -Be 0
-
-        # Clean up
-        ch360 delete classifier $classifierName --id="$ClientId" --secret="$ClientSecret"
     }
 
     It "should output failure when attempting to create a classifier that already exists" {
-        $classifierName = "test-classifier"
         # Run delete classifier first to ensure it's not already present
         ch360 create classifier $classifierName --id="$ClientId" --secret="$ClientSecret"
         ch360 create classifier $classifierName --id="$ClientId" --secret="$ClientSecret" | Should -Be "Creating classifier '$classifierName'... [FAILED]"
@@ -38,8 +34,11 @@ Describe "ch360 create classifier" {
     }
 
     It "should output failure when client id or secret are invalid" {
-        $classifierName = "test-classifier"
         ch360 create classifier $classifierName --id="invalid-id" --secret="$ClientSecret" | Should -Be "Creating classifier '$classifierName'... [FAILED]"
         $LASTEXITCODE | Should -Be 1
+    }
+
+    AfterAll {
+        ch360 delete classifier $classifierName --id="$ClientId" --secret="$ClientSecret"
     }
 }
