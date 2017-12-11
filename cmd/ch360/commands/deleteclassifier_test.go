9b2e3c36e888c8315d3c1262d1bfb2e138b2c03e
Alastair Smith <github@alastairsmith.me.uk>
2017-12-11 17:45:48 +0000

    Test that errors are appropriately returned
    If client calls fail, the command should return the error.

--- cmd/ch360/commands/deleteclassifier_test.go
+++ cmd/ch360/commands/deleteclassifier_test.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"errors"
 	"github.com/CloudHub360/ch360.go/ch360"
 	"github.com/CloudHub360/ch360.go/cmd/ch360/commands/mocks"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
 )
@@ -33,6 +35,34 @@ func TestDeleteClassifier_Execute_Does_Not_Delete_The_Named_Classifier_When_It_D
 
 	classifiersClient.AssertCalled(t, "GetAll")
 	classifiersClient.AssertNotCalled(t, "Delete")
+}
+
+func TestDeleteClassifier_Execute_Returns_An_Error_If_The_Classifiers_Cannot_Be_Retrieved(t *testing.T) {
+	classifiersClient := new(mocks.DeleterGetter)
+	expected := errors.New("Failed")
+	classifiersClient.On("GetAll", mock.Anything).Return(
+		nil, expected)
+
+	sut := NewDeleteClassifier(classifiersClient)
+
+	actual := sut.Execute("sydney")
+
+	assert.Equal(t, expected, actual)
+	classifiersClient.AssertNotCalled(t, "Delete")
+}
+
+func TestDeleteClassifier_Execute_Returns_An_Error_If_The_Classifier_Cannot_Be_Deleted(t *testing.T) {
+	classifiersClient := new(mocks.DeleterGetter)
+	classifiersClient.On("GetAll", mock.Anything).Return(
+		AListOfClassifiers("charlie", "jo", "chris"), nil)
+	expected := errors.New("Failed")
+	classifiersClient.On("Delete", mock.Anything).Return(expected)
+
+	sut := NewDeleteClassifier(classifiersClient)
+
+	actual := sut.Execute("charlie")
+
+	assert.Equal(t, expected, actual)
 }
 
 func AListOfClassifiers(names ...string) interface{} {
