e98beed49619e46afe46264c5cabbb9da6015f01
Alastair Smith <github@alastairsmith.me.uk>
2017-12-11 17:01:54 +0000

    Test that the given classifier is deleted
    Mock the classifiers client and expect the Delete function to be called.

--- cmd/ch360/commands/deleteclassifier_test.go
+++ cmd/ch360/commands/deleteclassifier_test.go
@@ -1,1 +1,39 @@
 package commands
+
+import (
+	"github.com/CloudHub360/ch360.go/ch360"
+	"github.com/CloudHub360/ch360.go/cmd/ch360/commands/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestDeleteClassifier_Execute_Deletes_The_Named_Classifier_When_It_Exists(t *testing.T) {
+	classifiersClient := new(mocks.DeleterGetter)
+	classifiersClient.On("GetAll", mock.Anything).Return(
+		AListOfClassifiers("charlie", "jo", "chris"), nil)
+
+	classifiersClient.On("Delete", mock.Anything).Return(nil)
+
+	sut := NewDeleteClassifier(classifiersClient)
+
+	sut.Execute("charlie")
+
+	assert.Len(t, classifiersClient.Calls, 2)
+	call := classifiersClient.Calls[1]
+
+	assert.Len(t, call.Arguments, 1)
+	actual := call.Arguments[0]
+
+	assert.Equal(t, "charlie", actual)
+}
+
+func AListOfClassifiers(names ...string) interface{} {
+	expected := make(ch360.ClassifierList, len(names))
+
+	for index, name := range names {
+		expected[index] = ch360.Classifier{name}
+	}
+
+	return expected
+}
