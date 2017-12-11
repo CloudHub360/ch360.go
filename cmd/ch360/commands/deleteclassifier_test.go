0c872ad0ec7bd054935d6baac89c341e1fd3a283
Alastair Smith <github@alastairsmith.me.uk>
2017-12-11 17:01:54 +0000

    Test that the given classifier is not deleted
    Mock the classifiers client and expect the Delete function to not be called
    if the classifier does not exist.

--- cmd/ch360/commands/deleteclassifier_test.go
+++ cmd/ch360/commands/deleteclassifier_test.go
@@ -28,6 +28,21 @@ func TestDeleteClassifier_Execute_Deletes_The_Named_Classifier_When_It_Exists(t 
 	assert.Equal(t, "charlie", actual)
 }
 
+func TestDeleteClassifier_Execute_Does_Not_Delete_The_Named_Classifier_When_It_Does_Not_Exist(t *testing.T) {
+	classifiersClient := new(mocks.DeleterGetter)
+	classifiersClient.On("GetAll", mock.Anything).Return(
+		AListOfClassifiers("charlie", "jo", "chris"), nil)
+
+	classifiersClient.On("Delete", mock.Anything).Return(nil)
+
+	sut := NewDeleteClassifier(classifiersClient)
+
+	sut.Execute("sydney")
+
+	assert.Len(t, classifiersClient.Calls, 1)
+	assert.Equal(t, classifiersClient.Calls[0].Method, "GetAll")
+}
+
 func AListOfClassifiers(names ...string) interface{} {
 	expected := make(ch360.ClassifierList, len(names))
 
