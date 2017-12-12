fa969046d088837c1d41b441e3c1a2dac2c3bd81
Alastair Smith <github@alastairsmith.me.uk>
2017-12-12 12:23:46 +0000

    Add smoke tests for create with --samples-zip arg
    In order to have the tests fail appropriately, the new argument must be
    defined as optional for the moment.

--- test/ch360-classifier.Tests.ps1
+++ test/ch360-classifier.Tests.ps1
@@ -46,6 +46,46 @@ Describe "ch360 create classifier" {
         $LASTEXITCODE | Should -Be 1
     }
 
+    It "should train the classifier with the provided ZIP file" {
+        # Run delete classifier first to ensure it's not already present
+        ch360 delete classifier $classifierName --id="$ClientId" --secret="$ClientSecret"
+        $samples = (Join-Path $pwd "samples.zip")
+
+        ch360 create classifier $classifierName --id="$ClientId" --secret="$ClientSecret" --samples-zip=$samples `
+            | Should -Be `
+@"
+Creating classifier '$classifierName'... [OK]
+Adding samples from file '$samples'... [OK]
+"@
+
+        $LASTEXITCODE | Should -Be 0
+    }
+
+    It "should output failure when the sample ZIP file does not exist at the specified path" {
+        $samples = (Join-Path $pwd "non-existent.zip")
+        ch360 create classifier $classifierName --id="$ClientId" --secret="$ClientSecret" --samples-zip=$samples `
+            | Should -Be `
+@"
+Creating classifier '$classifierName'... [FAILED]
+The file '$samples' could not be found.
+"@
+
+        $LASTEXITCODE | Should -Be 1
+    }
+
+    It "should output failure when the sample ZIP file does not match the required format " {
+        $samples = (Join-Path $pwd "invalid.zip")
+        ch360 create classifier $classifierName --id="$ClientId" --secret="$ClientSecret" --samples-zip=$samples `
+            | Should -Be `
+@"
+Creating classifier '$classifierName'... [OK]
+Adding samples from file '$samples'... [FAILED]
+The samples file is invalid.
+"@
+
+        $LASTEXITCODE | Should -Be 1
+    }
+
     AfterAll {
         ch360 delete classifier $classifierName --id="$ClientId" --secret="$ClientSecret"
     }
