4ee7b54b9fbbc20092d8e48e5d39e6ab11cf9306
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-14 14:25:59 +0000

    Move configuration to separate package

--- cmd/ch360/commands/login.go
+++ cmd/ch360/commands/login.go
@@ -1,5 +1,7 @@
 package commands
 
+import "github.com/CloudHub360/ch360.go/config"
+
 type Login struct {
 	client Getter
 }
@@ -11,6 +13,6 @@ func NewLogin(client Getter) *Login {
 }
 
 func (cmd *Login) Execute(clientId string, clientSecret string) error {
-	config := NewConfiguration(clientId, clientSecret)
+	config := config.NewConfiguration(clientId, clientSecret)
 	return config.Save()
 }
