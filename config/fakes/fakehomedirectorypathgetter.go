2f649a6fa5faf0d043190011134b6d571c55d73e
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-17 17:57:10 +0000

    Create test/output directory if necessary

--- config/fakes/fakehomedirectorypathgetter.go
+++ config/fakes/fakehomedirectorypathgetter.go
@@ -9,9 +9,17 @@ type FakeHomeDirectoryPathGetter struct {
 	Guid string
 }
 
-func (provider *FakeHomeDirectoryPathGetter) GetPath() string {
+func (dir *FakeHomeDirectoryPathGetter) GetPath() string {
 	path := filepath.Join(
 		os.Getenv("GOPATH"),
-		"src", "github.com", "CloudHub360", "ch360.go", "test", "output", provider.Guid)
+		"src", "github.com", "CloudHub360", "ch360.go", "test", "output", dir.Guid)
 	return path
+}
+
+func (dir *FakeHomeDirectoryPathGetter) Create() {
+	os.MkdirAll(dir.GetPath(), 600)
+}
+
+func (dir *FakeHomeDirectoryPathGetter) Destroy() {
+	os.RemoveAll(dir.GetPath())
 }
