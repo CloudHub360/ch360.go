acd4c13f11a57c05ebebd715532cc912a0d2349a
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-18 20:00:12 +0000

    Prompt user for secret if not set as parameter

--- cmd/ch360/commands/tests/login_test.go
+++ cmd/ch360/commands/tests/login_test.go
@@ -1,44 +1,74 @@
-package commands
+package tests
 
 import (
+	"github.com/CloudHub360/ch360.go/cmd/ch360/commands"
+	cmdmocks "github.com/CloudHub360/ch360.go/cmd/ch360/commands/mocks"
 	"github.com/CloudHub360/ch360.go/config"
-	//"github.com/CloudHub360/ch360.go/config/mocks"
+	"github.com/CloudHub360/ch360.go/config/mocks"
 	"github.com/stretchr/testify/assert"
-	//"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
 type LoginSuite struct {
 	suite.Suite
-	sut *Login
-	//configDirectory *config.ConfigurationWriter
-	configDirectory *config.ConfigurationWriter
+	sut             *commands.Login
+	configDirectory *mocks.ConfigurationWriter
+	secretReader    *cmdmocks.SecretReader
+	clientId        string
+	clientSecret    string
 }
 
 func (suite *LoginSuite) SetupTest() {
-	//suite.configDirectory = &fakeConfigurationWriter{}
-	//suite.configDirectory = new(mocks.ConfigurationWriter)
-	//suite.configDirectory.On("WriteConfiguration", mock.Anything).Return(0, nil)
+	suite.configDirectory = new(mocks.ConfigurationWriter)
+	suite.configDirectory.On("WriteConfiguration", mock.Anything).Return(nil)
 
-	suite.sut = NewLogin(suite.configDirectory)
+	suite.secretReader = new(cmdmocks.SecretReader)
+	suite.secretReader.On("Read").Return(suite.clientSecret, nil)
+	suite.sut = commands.NewLogin(suite.configDirectory, suite.secretReader)
+
+	suite.clientId = suite.clientId
+	suite.clientSecret = suite.clientSecret
 }
 
 func TestLoginSuiteRunner(t *testing.T) {
 	suite.Run(t, new(LoginSuite))
 }
 
-func (suite *LoginSuite) TestLogin_Execute_Writes_Configuration_To_ConfigurationDirectory() {
-	err := suite.sut.Execute("clientid", "clientsecret")
+func (suite *LoginSuite) TestLogin_Execute_Writes_Configuration_When_Id_And_Secret_Specified() {
+	err := suite.sut.Execute(suite.clientId, suite.clientSecret)
 	if err != nil {
 		assert.Error(suite.T(), err)
 	}
 
-	//suite.configDirectory.AssertCalled(suite.T(), "WriteConfiguration", mock.Anything)
+	suite.assertConfigurationWrittenWithCredentials(suite.clientId, suite.clientSecret)
 }
 
-type fakeConfigurationWriter struct{}
+func (suite *LoginSuite) TestLogin_Execute_Prompts_For_Secret_When_Secret_Not_Specified() {
+	err := suite.sut.Execute(suite.clientId, "")
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
 
-func (writer *fakeConfigurationWriter) WriteConfiguration(configuration *config.Configuration) error {
-	return nil
+	suite.secretReader.AssertCalled(suite.T(), "Read")
+}
+
+func (suite *LoginSuite) TestLogin_Execute_Writes_Configuration_When_Secret_Not_Specified() {
+	err := suite.sut.Execute(suite.clientId, "")
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
+
+	suite.assertConfigurationWrittenWithCredentials(suite.clientId, suite.clientSecret)
+}
+
+func (suite *LoginSuite) assertConfigurationWrittenWithCredentials(clientId string, clientSecret string) {
+	suite.configDirectory.AssertCalled(suite.T(), "WriteConfiguration", mock.Anything)
+
+	call := suite.configDirectory.Calls[0]
+	assert.Len(suite.T(), call.Arguments, 1)
+	configuration := call.Arguments[0].(*config.Configuration)
+	assert.Equal(suite.T(), clientId, configuration.ConfigurationRoot.Credentials[0].Id)
+	assert.Equal(suite.T(), clientSecret, configuration.ConfigurationRoot.Credentials[0].Secret)
 }
