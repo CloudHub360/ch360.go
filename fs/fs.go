3cb00ca7fe06c048bcf458f4bbcad2fa31b64411
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-19 16:12:28 +0000

    Move duplicated directoryExists functions into common package

---
+++ fs/fs.go
@@ -0,0 +1,31 @@
+package fs
+
+import "os"
+
+func DirectoryOrFileExists(dir string) (bool, error) {
+	_, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// directory does not exist
+			return false, nil
+		} else {
+			// other error
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+func CreateDirectoryIfNotExists(dir string, permissions os.FileMode) error {
+	exists, err := DirectoryOrFileExists(dir)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		err := os.Mkdir(dir, permissions)
+		return err
+	}
+	return nil
+}
