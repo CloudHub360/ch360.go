d60860512131fe402c996aaeed70f56363301bf6
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-19 14:31:12 +0000

    Move guid creation into fake
    It doesn't need to be exposed publically, and impedes readability

--- config/fakes/fakehomedirectorypathgetter.go
+++ config/fakes/fakehomedirectorypathgetter.go
@@ -1,21 +1,29 @@
 package fakes
 
 import (
+	"fmt"
 	"github.com/CloudHub360/ch360.go/config"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type FakeHomeDirectoryPathGetter struct {
-	Guid string
+	guid string
 	path string
+}
+
+func NewFakeHomeDirectoryPathGetter() *FakeHomeDirectoryPathGetter {
+	return &FakeHomeDirectoryPathGetter{
+		guid: fmt.Sprintf("%v", time.Now().UTC().UnixNano()),
+	}
 }
 
 func (dir *FakeHomeDirectoryPathGetter) Path() string {
 	if dir.path == "" {
 		tmpDir, _ := ioutil.TempDir("", "fakehome")
-		dir.path = filepath.Join(tmpDir, dir.Guid)
+		dir.path = filepath.Join(tmpDir, dir.guid)
 	}
 	return dir.path
 }
