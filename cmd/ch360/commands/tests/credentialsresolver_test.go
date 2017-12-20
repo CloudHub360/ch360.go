6a07c517c3a98e5bfb923d7645ba96ef77d5f764
Mark Wightman <mark.wightman@cloudhub360.com>
2017-12-20 10:58:14 +0000

    Add credentialsresolver type and tests

---
+++ cmd/ch360/commands/tests/credentialsresolver_test.go
@@ -0,0 +1,127 @@
+package tests
+
+import (
+	"errors"
+	"github.com/CloudHub360/ch360.go/config"
+	"github.com/CloudHub360/ch360.go/config/mocks"
+	"github.com/CloudHub360/ch360.go/test/generators"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type CredentialsResolverSuite struct {
+	suite.Suite
+	sut                *config.CredentialsResolver
+	appDirectory       *mocks.ConfigurationReader
+	configClientId     string
+	configClientSecret string
+}
+
+func (suite *CredentialsResolverSuite) SetupTest() {
+	suite.configClientId = generators.String("config-clientid")
+	suite.configClientSecret = generators.String("config-clientsecret")
+
+	configuration := config.NewConfiguration(suite.configClientId, suite.configClientSecret)
+	suite.appDirectory = new(mocks.ConfigurationReader)
+	suite.appDirectory.On("ReadConfiguration").Return(configuration, nil)
+
+	suite.sut = &config.CredentialsResolver{}
+}
+
+func TestCredentialsResolverSuiteRunner(t *testing.T) {
+	suite.Run(t, new(CredentialsResolverSuite))
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Parameters_If_Both_Set() {
+	clientIdParam := generators.String("clientid")
+	clientSecretParam := generators.String("clientsecret")
+
+	clientIdActual, clientSecretActual, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), clientIdParam, clientIdActual)
+	assert.Equal(suite.T(), clientSecretParam, clientSecretActual)
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Id_Parameter_And_Config_Secret_If_Only_Id_Set() {
+	clientIdParam := generators.String("clientid")
+	clientSecretParam := ""
+
+	clientIdActual, clientSecretActual, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), clientIdParam, clientIdActual)
+	assert.Equal(suite.T(), suite.configClientSecret, clientSecretActual)
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Secret_Parameter_And_Config_Id_If_Only_Secret_Set() {
+	clientIdParam := ""
+	clientSecretParam := generators.String("clientsecret")
+
+	clientIdActual, clientSecretActual, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), suite.configClientId, clientIdActual)
+	assert.Equal(suite.T(), clientSecretParam, clientSecretActual)
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Config_Values_If_Neither_Secret_Nor_Id_Set() {
+	clientIdParam := ""
+	clientSecretParam := ""
+
+	clientIdActual, clientSecretActual, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), suite.configClientId, clientIdActual)
+	assert.Equal(suite.T(), suite.configClientSecret, clientSecretActual)
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Error_If_Config_Values_Are_Needed_And_Id_Is_Empty() {
+	clientIdParam := ""
+	clientSecretParam := ""
+
+	suite.appDirectory.ExpectedCalls = nil
+	configuration := config.NewConfiguration("", suite.configClientSecret)
+	suite.appDirectory.On("ReadConfiguration").Return(configuration, nil)
+
+	_, _, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.NotNil(suite.T(), err)
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Error_If_Config_Values_Are_Needed_And_Secret_Is_Empty() {
+	clientIdParam := ""
+	clientSecretParam := ""
+
+	suite.appDirectory.ExpectedCalls = nil
+	configuration := config.NewConfiguration(suite.configClientId, "")
+	suite.appDirectory.On("ReadConfiguration").Return(configuration, nil)
+
+	_, _, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.NotNil(suite.T(), err)
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Error_If_Config_Values_Are_Needed_And_Contains_No_Credentials() {
+	clientIdParam := ""
+	clientSecretParam := ""
+	expectedErr := errors.New("Your configuration file does not contain any credentials. Please run ch360 login.")
+
+	suite.appDirectory.ExpectedCalls = nil
+	var credentials = make(config.ApiCredentialsList, 0)
+	configuration := &config.Configuration{
+		Credentials: credentials,
+	}
+	suite.appDirectory.On("ReadConfiguration").Return(configuration, nil)
+
+	_, _, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.NotNil(suite.T(), err)
+	assert.Equal(suite.T(), expectedErr, err)
+}
+
+func (suite *CredentialsResolverSuite) TestResolve_Returns_Error_If_ConfigurationReader_Returns_An_Error() {
+	clientIdParam := ""
+	clientSecretParam := ""
+
+	suite.appDirectory.ExpectedCalls = nil
+	expectedError := errors.New("Simulated config reading error")
+	suite.appDirectory.On("ReadConfiguration").Return(nil, expectedError)
+
+	_, _, err := suite.sut.Resolve(clientIdParam, clientSecretParam, suite.appDirectory)
+	assert.Equal(suite.T(), expectedError, err)
+}
