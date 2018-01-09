8ab32e4c4f0f469e9105b0112eedade9d6a868a4
Alastair Smith <github@alastairsmith.me.uk>
2018-01-09 13:50:16 +0000

    Add token expiry check
    Parse the JWT returned by TokenRetriever, and cache the parsed object.
    Check the parsed token in the cache to see if it has expired before
    refreshing it.

--- auth/httptokencache.go
+++ auth/httptokencache.go
@@ -1,10 +1,16 @@
 package auth
 
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
 type tokenCache struct {
 	retriever TokenRetriever
 }
 
-var cachedToken string
+var cachedToken *jwt.Token
 
 func newHttpTokenCache(tokenRetriever TokenRetriever) TokenRetriever {
 	return &tokenCache{
@@ -13,10 +19,49 @@ func newHttpTokenCache(tokenRetriever TokenRetriever) TokenRetriever {
 }
 
 func (cache *tokenCache) RetrieveToken() (string, error) {
-	var err error = nil
-	if cachedToken == "" {
-		cachedToken, err = cache.retriever.RetrieveToken()
+	if tokenIsFresh(cachedToken) {
+		return cachedToken.Raw, nil
 	}
 
-	return cachedToken, err
+	tokenString, err := cache.retriever.RetrieveToken()
+	if err != nil {
+		return "", err
+	}
+
+	token, err := parseToken(tokenString)
+	if err != nil {
+		return "", err
+	}
+
+	cachedToken = token
+	return cachedToken.Raw, nil
+}
+
+func parseToken(token string) (*jwt.Token, error) {
+	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		kid := token.Header["kid"]
+		if kid != nil {
+			return kid, nil
+		}
+
+		return []byte("secret"), nil
+	})
+}
+
+func tokenIsFresh(token *jwt.Token) bool {
+	if token == nil || !token.Valid {
+		return false
+	}
+
+	var expired = false
+	if claims, ok := token.Claims.(*jwt.MapClaims); ok {
+		deadline := time.Now().Add(-1 * time.Minute)
+		expired = claims.VerifyExpiresAt(int64(deadline.Unix()), true)
+	}
+
+	return !expired
 }
