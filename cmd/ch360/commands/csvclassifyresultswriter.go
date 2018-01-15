12b00df226fae3d9b81af7f07134b017f2b913db
Mark Wightman <mark.wightman@cloudhub360.com>
2018-01-15 13:31:55 +0000

    Use csv package to write csv results

--- cmd/ch360/commands/csvclassifyresultswriter.go
+++ cmd/ch360/commands/csvclassifyresultswriter.go
@@ -1,26 +1,38 @@
 package commands
 
 import (
-	"fmt"
+	"encoding/csv"
 	"io"
 )
 
 type CSVClassifyResultsWriter struct {
-	writer io.Writer
+	underlyingWriter io.Writer
+	csvWriter        *csv.Writer
 }
 
 func NewCSVClassifyResultsWriter(writer io.Writer) *CSVClassifyResultsWriter {
 	return &CSVClassifyResultsWriter{
-		writer: writer,
+		underlyingWriter: writer,
 	}
 }
 
-var classifyCSVOutputFormat = "%s,%s,%v\n"
-
-func (writer *CSVClassifyResultsWriter) StartWriting() {}
+func (writer *CSVClassifyResultsWriter) StartWriting() {
+	writer.csvWriter = csv.NewWriter(writer.underlyingWriter)
+}
 
 func (writer *CSVClassifyResultsWriter) WriteDocumentResults(result *classifyResultsWriterInput) {
-	fmt.Fprintf(writer.writer, classifyCSVOutputFormat, result.filename, result.documentType, result.isConfident)
+	record := []string{result.filename, result.documentType, boolToString(result.isConfident)}
+
+	writer.csvWriter.Write(record) //TODO check error
+	writer.csvWriter.Flush()
 }
 
 func (writer *CSVClassifyResultsWriter) FinishWriting() {}
+
+func boolToString(val bool) string {
+	if val {
+		return "true"
+	} else {
+		return "false"
+	}
+}
