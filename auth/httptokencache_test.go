71e0b42c545ab4238cc73a488c781e90158eb7b3
Phil Richards <phil.richards@cloudhub360.com>
2018-01-15 13:51:39 +0000

    `auth.TokenCache` is now safe to use in parallel
    Multiple goroutines can now access the `auth.TokenCache` without the
    possibility of lots of requests being fired to get a new token.
    
    Also fixed up the `tokenIsFresh` function and the `expiredToken` and
    `validToken` variables used in the unit tests.

--- auth/httptokencache_test.go
+++ auth/httptokencache_test.go
@@ -6,15 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"sync"
 	"testing"
 	"time"
 )
 
-var validToken = auth.AccessToken{
+var expiredToken = auth.AccessToken{
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImV4cCI6IjE1MTU0MTE5ODIifQ.efsGk6oZDo3PK5euKvuoa-KDHcXY5gQUGdoeN-OO9LA",
 	time.Date(2018, time.Month(1), 8, 11, 46, 22, 0, &time.Location{}),
 }
-var expiredToken = auth.AccessToken{
+var validToken = auth.AccessToken{
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImV4cCI6IjQ2NzExNzE5ODIifQ.vsp4YkbzAwwogc9qCjwjICSXqVARjVKL6neEm7iHnYY",
 	time.Date(2118, time.Month(1), 9, 11, 46, 22, 0, &time.Location{}),
 }
@@ -53,6 +54,23 @@ func (suite *tokenCacheSuite) Test_RetrieveToken_Requests_New_Token_If_It_Has_Ex
 	assert.Nil(suite.T(), err)
 	assert.Equal(suite.T(), &validToken, token)
 	suite.tokenRetriever.AssertNumberOfCalls(suite.T(), "RetrieveToken", 2)
+}
+
+func (suite *tokenCacheSuite) Test_RetrieveToken_Only_Requests_New_Token_Once_If_Used_In_Parallel() {
+	suite.reStub("RetrieveToken").Return(&validToken, nil)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			suite.sut.RetrieveToken()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	suite.tokenRetriever.AssertNumberOfCalls(suite.T(), "RetrieveToken", 1)
 }
 
 func (suite *tokenCacheSuite) populateCache(token auth.AccessToken) {
