a1e4e2566d64162da8f7d734c87d549edaf9f42b
Mark Wightman <mark.wightman@cloudhub360.com>
2018-01-15 17:12:22 +0000

    Check that StartWriting has been called in csv writer

--- cmd/ch360/commands/csvclassifyresultswriter.go
+++ cmd/ch360/commands/csvclassifyresultswriter.go
@@ -3,12 +3,14 @@ package commands
 import (
 	"encoding/csv"
 	"github.com/CloudHub360/ch360.go/ch360/types"
+	"github.com/pkg/errors"
 	"io"
 )
 
 type CSVClassifyResultsWriter struct {
 	underlyingWriter io.Writer
 	csvWriter        *csv.Writer
+	startCalled      bool
 }
 
 func NewCSVClassifyResultsWriter(writer io.Writer) *CSVClassifyResultsWriter {
@@ -19,9 +21,14 @@ func NewCSVClassifyResultsWriter(writer io.Writer) *CSVClassifyResultsWriter {
 
 func (writer *CSVClassifyResultsWriter) StartWriting() {
 	writer.csvWriter = csv.NewWriter(writer.underlyingWriter)
+	writer.startCalled = true
 }
 
 func (writer *CSVClassifyResultsWriter) WriteDocumentResults(filename string, result *types.ClassificationResult) error {
+	if !writer.startCalled {
+		return errors.New("StartWriting() must be called before WriteDocumentResults()")
+	}
+
 	record := []string{filename, result.DocumentType, boolToString(result.IsConfident)}
 
 	if err := writer.csvWriter.Write(record); err != nil {
