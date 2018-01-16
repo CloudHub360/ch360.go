1da7f28aaeb7ffdb2e0624b2425d155aeb68db02
Mark Wightman <mark.wightman@cloudhub360.com>
2018-01-16 13:20:47 +0000

    Include document type scores in classification result

--- ch360/types/types.go
+++ ch360/types/types.go
@@ -4,4 +4,10 @@ type ClassificationResult struct {
 	DocumentType       string
 	IsConfident        bool
 	RelativeConfidence float64
+	DocumentTypeScores []DocumentTypeScore
+}
+
+type DocumentTypeScore struct {
+	DocumentType string
+	Score        float64
 }
