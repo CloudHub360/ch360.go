16f198b4166cf3f1a5579b3c1adfc2fe00ccd9fa
Mark Wightman <mark.wightman@cloudhub360.com>
2018-01-17 17:22:09 +0000

    Update version smoke tests to use surf

--- test/ch360-version.Tests.ps1
+++ test/ch360-version.Tests.ps1
@@ -1,17 +1,17 @@
-Describe "ch360 --version" {
+Describe "surf --version" {
   It "should output date of release" {
-    ch360 --version | Should -Match "\d{4}\.\d{2}\.\d{2}"
+    surf --version | Should -Match "\d{4}\.\d{2}\.\d{2}"
   }
 
   It "should output short git hash" {
-    ch360 --version | Should -Match ".*-([a-f0-9]{7,})"
+    surf --version | Should -Match ".*-([a-f0-9]{7,})"
   }
 
   It "should output build number" {
-    ch360 --version | Should -Match ".*-(\d+)"
+    surf --version | Should -Match ".*-(\d+)"
   }
 
   It "should output the supported format" {
-    ch360 --version | Should -Match "\d{4}\.\d{2}\.\d{2}-([a-f0-9]{7,})-(\d+)"
+    surf --version | Should -Match "\d{4}\.\d{2}\.\d{2}-([a-f0-9]{7,})-(\d+)"
   }
 }
