1802bd1e200875daf1407d84b220fa46c6fbd717
Mark Wightman <mark.wightman@cloudhub360.com>
2018-01-17 14:11:43 +0000

    Remove 'interrupted' message when cancelling login command

--- cmd/ch360/commands/consolesecretreader.go
+++ cmd/ch360/commands/consolesecretreader.go
@@ -1,11 +1,17 @@
 package commands
 
-import "github.com/howeyc/gopass"
+import (
+	"github.com/howeyc/gopass"
+)
 
-type ConsoleSecretReader struct{}
+type ConsoleSecretReader struct {
+}
+
+var ConsoleSecretReaderErrCancelled = gopass.ErrInterrupted
 
 func (reader *ConsoleSecretReader) Read() (string, error) {
 	secret, err := gopass.GetPasswd()
+
 	if err != nil {
 		return "", err
 	}
