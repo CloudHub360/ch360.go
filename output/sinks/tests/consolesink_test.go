09045bbed4a52e6f7186bbee02ccfa7af053276f
Mark Wightman <mark.wightman@cloudhub360.com>
2018-01-19 14:25:30 +0000

    Add tests for consoleSink

---
+++ output/sinks/tests/consolesink_test.go
@@ -0,0 +1,63 @@
+package tests
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/CloudHub360/ch360.go/output/sinks"
+	"github.com/CloudHub360/ch360.go/test/generators"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ConsoleSinkSuite struct {
+	suite.Suite
+	sut      *sinks.ConsoleSink
+	contents string
+	output   *bytes.Buffer
+}
+
+func (suite *ConsoleSinkSuite) SetupTest() {
+	suite.output = &bytes.Buffer{}
+	suite.contents = generators.String("contents")
+
+	suite.sut = sinks.NewConsoleSink(suite.output)
+}
+
+func TestConsoleSinkRunner(t *testing.T) {
+	suite.Run(t, new(ConsoleSinkSuite))
+}
+
+func (suite *ConsoleSinkSuite) TestOpen_Returns_Nil() {
+	err := suite.sut.Open()
+
+	assert.Nil(suite.T(), err)
+}
+
+func (suite *ConsoleSinkSuite) TestClose_Returns_Nil() {
+	err := suite.sut.Open()
+
+	assert.Nil(suite.T(), err)
+}
+
+func (suite *ConsoleSinkSuite) TestWrite_Delegates_To_Writer() {
+	_, err := fmt.Fprint(suite.sut, suite.contents)
+
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), suite.contents, suite.output.String())
+}
+
+func (suite *ConsoleSinkSuite) TestWrite_Returns_Error_From_Writer() {
+	sut := sinks.NewConsoleSink(&erroringWriter{})
+	length, err := fmt.Fprint(sut, suite.contents)
+
+	assert.NotNil(suite.T(), err)
+	assert.Equal(suite.T(), 0, length)
+}
+
+type erroringWriter struct{}
+
+func (w *erroringWriter) Write(b []byte) (int, error) {
+	return 0, errors.New("simulated error")
+}
