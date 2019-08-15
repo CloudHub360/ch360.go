9e0a5a82d14610e7511f454a5efc017f6db791d4
Phil Richards <phil@plottwist.io>
2019-08-15 17:04:47 +0100

    Extract ParallelFilesReader service
    Previously within ReadCmd

--- cmd/surf/commands/read.go
+++ cmd/surf/commands/read.go
@@ -2,196 +2,86 @@ package commands
 
 import (
 	"context"
-	"errors"
-	"fmt"
+	"github.com/CloudHub360/ch360.go/cmd/surf/services"
 	"github.com/CloudHub360/ch360.go/output/resultsWriters"
-	"io"
+	"github.com/pkg/errors"
 	"os"
 
 	"github.com/CloudHub360/ch360.go/ch360"
 	"github.com/CloudHub360/ch360.go/config"
 	"github.com/CloudHub360/ch360.go/output/progress"
-	"github.com/CloudHub360/ch360.go/pool"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-const ReadFilesCommand = "read"
-
-//go:generate mockery -name "FileReader|FilesProcessor"
-
-type FileReader interface {
-	Read(ctx context.Context, fileContent io.Reader, mode ch360.ReadMode) (io.ReadCloser, error)
-}
-
-type FilesProcessor interface {
-	RunWithGlob(ctx context.Context,
-		filePatterns []string,
-		parallelism int,
-		processorFuncFactory ProcessorFuncFactory) error
-}
-
-type ReadCmd struct {
-	fileReader FileReader
-	//parallelWorkers int
-	//filesProcessor  FilesProcessor
+type ReadArgs struct {
 	outputFormat string
 	filePatterns []string
-	globalFlags  *config.GlobalFlags
 }
 
+// ConfigureReadCommand configures kingpin to call ExecuteRead after having successfully parsed
+// the cli options.
 func ConfigureReadCommand(ctx context.Context,
 	app *kingpin.Application,
 	globalFlags *config.GlobalFlags) {
-	readCmd := &ReadCmd{
-		globalFlags: globalFlags,
-	}
+	readArgs := &ReadArgs{}
+
 	cmd := app.
 		Command("read", "Perform OCR on a file or set of files.").
 		Action(func(parseContext *kingpin.ParseContext) error {
-			return readCmd.Execute(ctx)
+			// execute the command
+			return ExecuteRead(ctx, readArgs, globalFlags)
 		})
 
 	cmd.Arg("format", "The output format. Allowed values: pdf, wvdoc, txt.").
 		Required().
-		EnumVar(&readCmd.outputFormat, "pdf", "wvdoc", "txt")
+		EnumVar(&readArgs.outputFormat, "pdf", "wvdoc", "txt")
 
 	cmd.Arg("files", "The files to read.").
 		Required().
-		StringsVar(&readCmd.filePatterns)
+		StringsVar(&readArgs.filePatterns)
 }
 
-func (cmd *ReadCmd) processorFor(ctx context.Context, filename string,
-	readMode ch360.ReadMode) pool.ProcessorFunc {
-	return func() (interface{}, error) {
-		file, err := os.Open(filename)
-		if err != nil {
-			return nil, wrapErr(filename, err)
-		}
-
-		readCloser, err := cmd.fileReader.Read(ctx, file, readMode)
-
-		return readCloser, wrapErr(filename, err)
-	}
-}
-
-func wrapErr(filename string, err error) error {
-	if err == nil {
-		return nil
-	}
-	return errors.New(fmt.Sprintf("Error reading file %s: %v", filename, err))
-}
-
-func (cmd *ReadCmd) Execute(ctx context.Context) error {
-	client, err := initApiClient(cmd.globalFlags.ClientId, cmd.globalFlags.ClientSecret, cmd.globalFlags.LogHttp)
+// ExecuteRead is the main entry point for the 'read' command. It has to do a lot of
+// setup / instantiation before actually performing OCR on the specified files.
+func ExecuteRead(ctx context.Context, readArgs *ReadArgs, globalFlags *config.GlobalFlags) error {
+	client, err := initApiClient(globalFlags.ClientId, globalFlags.ClientSecret, globalFlags.LogHttp)
 
 	if err != nil {
 		return err
 	}
 
-	fileExtension := fmt.Sprintf(".ocr.%s", cmd.outputFormat)
+	fileExtension := ".ocr." + readArgs.outputFormat
 
-	resultsWriter, err := resultsWriters.NewResultsWriterFor(cmd.globalFlags, fileExtension,
+	resultsWriter, err := resultsWriters.NewResultsWriterFor(globalFlags, fileExtension,
 		config.Read)
 
-	progressHandler := progress.NewProgressHandler(resultsWriter,
-		cmd.globalFlags.ShowProgress, os.Stderr)
-
 	if err != nil {
 		return err
 	}
 
-	filesProcessor := &ParallelFilesProcessor{ProgressHandler: progressHandler}
+	progressHandler := progress.NewProgressHandler(resultsWriter,
+		globalFlags.ShowProgress, os.Stderr)
 
-	var readMode = ch360.ReadText
-	switch cmd.outputFormat {
-	case "wvdoc":
-		readMode = ch360.ReadWvdoc
-	case "pdf":
-		readMode = ch360.ReadPDF
-	}
+	readMode := readModes[readArgs.outputFormat]
 
 	// ensure we're not printing binary data to the console
 	if !config.IsOutputRedirected() &&
 		readMode.IsBinary() &&
-		!cmd.globalFlags.IsOutputSpecified() {
+		!globalFlags.IsOutputSpecified() {
 		return errors.New("you must use '-o' or '-m' or redirect stdout when the output " +
-			"file format is pdf")
+			"file format is pdf or wvdoc")
 	}
 
-	cmd.fileReader = ch360.NewFileReader(client.Documents, client.Documents, client.Documents)
+	filePaths := GlobMany(readArgs.filePatterns)
 
-	// Get the current number of documents, so we know how many slots are available
-	docs, err := client.Documents.GetAll(ctx)
-	if err != nil {
-		return err
-	}
+	fileReader := ch360.NewFileReader(client.Documents, client.Documents, client.Documents)
 
-	// Limit the number of workers to the number of available doc slots
-	parallelWorkers := min(10, ch360.TotalDocumentSlots-len(docs))
-
-	return filesProcessor.RunWithGlob(ctx,
-		cmd.filePatterns,
-		parallelWorkers,
-		func(ctx context.Context, filename string) pool.ProcessorFunc {
-			return cmd.processorFor(ctx, filename, readMode)
-		})
+	return services.NewParallelReaderService(fileReader, client.Documents, progressHandler).
+		ReadAll(ctx, filePaths, readMode)
 }
 
-//
-//func NewReadFilesCommand(reader FileReader,
-//	processor FilesProcessor,
-//	mode ch360.ReadMode,
-//	filePatterns []string,
-//	parallelWorkers int,
-//	getter ch360.DocumentGetter) *ReadCmd {
-//	return &ReadCmd{
-//		filesProcessor:  processor,
-//		fileReader:      reader,
-//		documentGetter:  getter,
-//		parallelWorkers: parallelWorkers,
-//		filePatterns:    filePatterns,
-//		mode:            mode,
-//	}
-//}
-//
-//func NewReadFilesCommandFromArgs(params *config.GlobalFlags, client *ch360.ApiClient, readFormat string, filePatterns []string) (*ReadCmd, error) {
-//
-//	progressHandler, err := progress.NewProgressHandlerFor(params, os.Stderr)
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	filesProcessor := &ParallelFilesProcessor{
-//		ProgressHandler: progressHandler,
-//	}
-//
-//	var readMode = ch360.ReadText
-//	switch readFormat {
-//	case "wvdoc":
-//		readMode = ch360.ReadWvdoc
-//	case "pdf":
-//		readMode = ch360.ReadPDF
-//	}
-//
-//	// ensure we're not printing binary data to the console
-//	if !config.IsOutputRedirected() &&
-//		readMode.IsBinary() &&
-//		!params.IsOutputSpecified() {
-//		return nil, errors.New("You must use '-o' or '-m' or redirect stdout when the output " +
-//			"file format is pdf.")
-//	}
-//
-//	fileReader := ch360.NewFileReader(client.Documents, client.Documents, client.Documents)
-//	return NewReadFilesCommand(fileReader,
-//		filesProcessor,
-//		readMode,
-//		filePatterns,
-//		10,
-//		client.Documents), nil
-//
-//}
-
-func (cmd ReadCmd) Usage() string {
-	return ReadFilesCommand
+var readModes = map[string]ch360.ReadMode{
+	"wvdoc": ch360.ReadWvdoc,
+	"pdf":   ch360.ReadPDF,
+	"txt":   ch360.ReadText,
 }
---
+++ cmd/surf/services/parallelreader.go
@@ -0,0 +1,75 @@
+package services
+
+import (
+	"context"
+	"github.com/CloudHub360/ch360.go/ch360"
+	"github.com/CloudHub360/ch360.go/pool"
+	"github.com/pkg/errors"
+	"io"
+	"os"
+)
+
+//go:generate mockery -name "FileReader|FileGlobProcessor"
+
+type FileReader interface {
+	Read(ctx context.Context, fileContent io.Reader, mode ch360.ReadMode) (io.ReadCloser, error)
+}
+
+type FileGlobProcessor interface {
+	RunWithGlob(ctx context.Context,
+		filePatterns []string,
+		parallelism int,
+		processorFuncFactory ProcessorFuncFactory) error
+}
+
+// ParallelReaderService wraps the ch360.FileReader to process multiple files in parallel.
+type ParallelReaderService struct {
+	singleFileReader       FileReader
+	documentGetter         ch360.DocumentGetter
+	parallelFilesProcessor ParallelFilesProcessor
+}
+
+// NewParallelReaderService constructs a new ParallelReaderService.
+func NewParallelReaderService(fileReader FileReader,
+	documentGetter ch360.DocumentGetter,
+	progressHandler ProgressHandler) *ParallelReaderService {
+
+	return &ParallelReaderService{
+		singleFileReader: fileReader,
+		documentGetter:   documentGetter,
+		parallelFilesProcessor: ParallelFilesProcessor{
+			ProgressHandler: progressHandler,
+		},
+	}
+}
+
+func (p *ParallelReaderService) ReadAll(ctx context.Context, files []string,
+	readMode ch360.ReadMode) error {
+
+	// Get the current number of documents, so we know how many slots are available
+	docs, err := p.documentGetter.GetAll(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Limit the number of workers to the number of available doc slots
+	parallelWorkers := ch360.TotalDocumentSlots - len(docs)
+
+	// called in parallel, once per file
+	processorFunc := func(ctx context.Context, filename string) pool.ProcessorFunc {
+		return func() (interface{}, error) {
+			file, err := os.Open(filename)
+			if err != nil {
+				return nil, errors.Wrapf(err, "Error reading file %s", filename)
+			}
+			defer file.Close()
+
+			readCloser, err := p.singleFileReader.Read(ctx, file, readMode)
+
+			return readCloser, errors.Wrapf(err, "Error reading file %s", filename)
+		}
+	}
+
+	return p.parallelFilesProcessor.Run(ctx, files, parallelWorkers,
+		processorFunc)
+}
