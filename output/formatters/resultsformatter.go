ffdc5753d8da6d3e43aa20782386447ea3633326
Phil Richards <phil@plottwist.io>
2019-08-16 14:27:19 +0100

    Remove unnecessary code
    Also add ResultsWriter ctors

--- output/formatters/resultsformatter.go
+++ output/formatters/resultsformatter.go
@@ -1,9 +1,6 @@
 package formatters
 
 import (
-	"errors"
-	"fmt"
-	"github.com/CloudHub360/ch360.go/config"
 	"io"
 )
 
@@ -26,37 +23,4 @@ const (
 type ResultsFormatter interface {
 	WriteResult(writer io.Writer, filename string, result interface{}, options FormatOption) error
 	Flush(writer io.Writer) error
-}
-
-func NewResultsFormatterFor(params *config.GlobalFlags, verb config.Verb) (ResultsFormatter, error) {
-	if verb == config.Read {
-		return NewReadResultsFormatter(), nil
-	}
-
-	var formatter ResultsFormatter
-	switch OutputFormat(params.OutputFormat) {
-	case Table:
-		if verb == config.Classify {
-			formatter = NewTableClassifyResultsFormatter()
-		} else {
-			formatter = NewTableExtractionResultsFormatter()
-		}
-	case Csv:
-		if verb == config.Classify {
-			formatter = NewCSVClassifyResultsFormatter()
-		} else {
-			formatter = NewCSVExtractionResultsFormatter()
-		}
-	case Json:
-		if verb == config.Classify {
-			formatter = NewJsonClassifyResultsFormatter()
-		} else {
-			formatter = NewJsonExtractionResultsFormatter()
-		}
-	default:
-		// DocOpt doesn't do validation of these values for us, so we need to catch invalid values here
-		return nil, errors.New(fmt.Sprintf("Unknown output format '%s'. Allowed values are: csv, table, json.", params.OutputFormat))
-	}
-
-	return formatter, nil
 }
